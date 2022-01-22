e79ce7293b32c5e4f55c258d7c38a01bac6e4072
Crazy boy <95070448+GodOfanjal@users.noreply.github.com>
2022-01-22 08:52:06 +0530

    Update backups.py

--- backups.py
+++ backups.py
@@ -375,10 +375,10 @@ __mod_name__ = "💌Backups"
 __help__ = """
 *Only for group owner:*
 
- ✪ /import: Reply to the backup file for the butler emilia group to import as much as possible, making transfers very easy! 
+ ❍ /import: Reply to the backup file for the butler emilia group to import as much as possible, making transfers very easy! 
  Note that files photos cannot be imported due to telegram restrictions.
 
- ✪ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ ❍ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
